cbb8c278ad08b47f15ed405d0fb1b72ad9d3bd05
201851028 <49832102+201851028@users.noreply.github.com>
2021-02-17 20:32:54 +0530

    website restaurant files

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*  ---------- SHOW MENU ----------- */
+
+// validate that variables exist
+
+// we add the show-menu class to the div tag with new__menu class
+
+
+
+/* ----- REMOVE MENU MOBILE --------- */
+    // when we click on each nav__link, we remove the show-menu class
+
+
+
+/* ------- SCROLL SECTIONS ACTIVE LINK ----- */
+
+/* ------- CHANGE BACKGROUND HEADER -------- */
+
+      //when the scroll is greater than the 200 viewport height,add the scroll-header class
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
